000d6995705dc905ca2565458565ae61585e7bc8
Brendan Dibbell <stego@stego.ai>
2026-03-31 14:34:43 -0400

    Workflow bugfix (#15)
    * maybe the real fix this time
    
    * fix local.py template file for remote runner

--- aligulac/aligulac/template.local.py
+++ aligulac/aligulac/template.local.py
@@ -1,45 +1,47 @@
+import os
+
+# Helper to get env or default
+def get_env(name, default=None):
+    return os.environ.get(name, default)
+
 # Path of the folder where manage.py is located
-PROJECT_PATH = '/home/eivind/repos/aligulac/aligulac/'
+PROJECT_PATH = get_env('PROJECT_PATH', '/app/aligulac/')
 
 # Path of folder where database dumps are saved
-# If you never call dump.py this is not necessary
-DUMP_PATH = '/home/eivind/repos/aligulac/untracked/'
+DUMP_PATH = get_env('DUMP_PATH', '/app/aligulac/untracked/')
 
 # Path of folder where the locales are stored
-LOCALE_PATHS = ('/home/eivind/repos/aligulac/locale/',)
+LOCALE_PATHS = (get_env('LOCALE_PATHS', '/app/aligulac/locale/'),)
 
-# Random string, 50 characters
-SECRET_KEY = ''
+# Secret Key
+SECRET_KEY = get_env('SECRET_KEY', '')
 
 # API key to openexchangerates.org
-EXCHANGE_ID = ''
+EXCHANGE_ID = get_env('EXCHANGE_ID', '')
 
-# Database name, host, username and password
-DB_NAME = 'aligulac'
-DB_HOST = '127.0.0.1'
-DB_USER = ''
-DB_PASSWORD = ''
+# Database name, host, port, username and password
+DB_NAME = get_env('DB_NAME', 'aligulac')
+DB_HOST = get_env('DB_HOST', '127.0.0.1')
+DB_PORT = get_env('DB_PORT', '5432')
+DB_USER = get_env('DB_USER', '')
+DB_PASSWORD = get_env('DB_PASSWORD', '')
 
 # Folder where the templates are stored
-TEMPLATE_DIRS = ('/home/eivind/repos/aligulac/templates/',)
+TEMPLATE_DIRS = (get_env('TEMPLATE_DIRS', '/app/templates/'),)
 
 # Host names this server accepts connections to
-ALLOWED_HOSTS = ['.aligulac.com', 'localhost']
+ALLOWED_HOSTS = get_env('ALLOWED_HOSTS', '.aligulac.com,localhost').split(',')
 
 # Necessary for django debug toolbar to work
-INTERNAL_IPS = ('127.0.0.1',)
+INTERNAL_IPS = get_env('INTERNAL_IPS', '127.0.0.1').split(',')
 
-# Cache backend (use DummyCache in development)
-CACHE_BACKEND = 'django.core.cache.backends.dummy.DummyCache'
+# Cache backend
+CACHE_BACKEND = get_env('CACHE_BACKEND', 'django.core.cache.backends.dummy.DummyCache')
+CACHE_LOCATION = get_env('CACHE_LOCATION', '/app/aligulac/untracked/cache/')
 
-# Cache location (where to store cached views with FileBasedCache, just leave empty if DummyCache)
-CACHE_LOCATION = '/home/eivind/repos/aligulac/untracked/cache/'
-
-# Debug mode (boolean, should be True in development)
-DEBUG = True
-
-# Include debug toolbar in debug mode?
-DEBUG_TOOLBAR = True
+# Debug mode
+DEBUG = get_env('DEBUG', 'True').lower() == 'true'
+DEBUG_TOOLBAR = get_env('DEBUG_TOOLBAR', 'True').lower() == 'true'
 
 # Log file for errors
-ERROR_LOG_FILE = '/home/eivind/repos/aligulac/untracked/error.log'
+ERROR_LOG_FILE = get_env('ERROR_LOG_FILE', '/var/log/aligulac/error.log')
