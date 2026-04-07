c27c34c9d03d69fc7bd3606625a08d3b455b3aaa
Brendan Dibbell <stego@stego.ai>
2026-04-07 18:28:12 -0400

    remove unused import

--- aligulac/recompute.py
+++ aligulac/recompute.py
@@ -12,7 +12,6 @@ import itertools
 import sys
 import subprocess
 
-from django.core.cache import cache
 from django.db import connection
 from django.db.models import F, Q
 from django.db.transaction import atomic
